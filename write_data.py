e2b4d15d28ba2bd5c7ebeb7c88dd3b9670776e40
Caismis <caismis@live.com>
2022-06-14 09:00:52 +0800

    NO update

--- write_data.py
+++ write_data.py
@@ -47,6 +47,7 @@ def write_data(input_data, uniset, misc):
 
 
 def write_remap(input_data, uniset, misc):
+    nogen = input_data[15]
     print('---Write data to file---')
     cseries = np.linspace(0, misc[2], len(uniset))
     optf = open('clist.d', 'w')
@@ -60,7 +61,7 @@ def write_remap(input_data, uniset, misc):
     npoint = input_data[12]
     zetas = np.linspace(0, 1, nzeta, endpoint=False)
     normzs = np.linspace(0, 1, npoint)
-    mapset = remapper(uniset, misc[0], npoint, nzeta)
+    mapset = remapper(uniset, misc[0], npoint, nzeta, nogen)
     optf = open(outname, 'w')
     optf.write('Max Value of C' + '\n')
     optf.write('{:<.6e}\n'.format(misc[2]))
@@ -103,7 +104,7 @@ def write_remap(input_data, uniset, misc):
     optf.close()
 
 
-def remapper(uniset, nsp, npoint, nzeta):
+def remapper(uniset, nsp, npoint, nzeta, nogen):
     print('---Remapping Data---')
     lambdas = len(uniset)
     normzs = np.linspace(0, 1, npoint)
@@ -117,17 +118,24 @@ def remapper(uniset, nsp, npoint, nzeta):
     #Species Fraction
     remapd = []
     #Density
+    if nogen == 'True':
+        remapno = []
+    #No Rate
     for i in range(len(normzs)):
         listt = []
         listc = []
         listr = []
         lists = []
         listd = []
+        if nogen == 'True':
+            listno = []
         for j in range(nzeta):
             slicet = np.empty(lambdas)
             slicec = np.empty(lambdas)
             slicer = np.empty(lambdas)
             sliced = np.empty(lambdas)
+            if nogen == 'True':
+                sliceno = np.empty(lambdas)
             slices =  []
             for isp in range(nsp):
                 slices.append(np.empty(lambdas))
@@ -139,6 +147,8 @@ def remapper(uniset, nsp, npoint, nzeta):
                 slicet[k] = uniset[cindex[k]][j][1][i]
                 slicer[k] = uniset[cindex[k]][j][5][i]
                 sliced[k] = uniset[cindex[k]][j][2][i]
+                if nogen == 'True':
+                    sliceno[k] = uniset[cindex[k]][j][6][i]
                 for isp in range(nsp):
                     slices[isp][k] = uniset[cindex[k]][j][0][isp][i]
             listt.append(slicet)
@@ -146,12 +156,19 @@ def remapper(uniset, nsp, npoint, nzeta):
             listr.append(slicer)
             lists.append(slices)
             listd.append(sliced)
+            if nogen == 'True':
+                listno.append(sliceno)
         remapt.append(listt)
         remapc.append(listc)
         remapr.append(listr)
         remaps.append(lists)
         remapd.append(listd)
-    return [remapc, remaps, remapt, remapr, remapd]
+        if nogen == 'True':
+            remapno.append(listno)
+    if nogen == 'True':
+        return [remapc, remaps, remapt, remapr, remapd, remapno]
+    else:
+        return [remapc, remaps, remapt, remapr, remapd]
 
 def cinterpol(clist, nlist, stdc):
     outlist = []
